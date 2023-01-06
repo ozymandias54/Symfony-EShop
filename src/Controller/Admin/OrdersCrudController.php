7b733ffa622539d00badb67d25034acaf9232c74
ozymandias54 <bassirou718@gmail.com>
2023-01-06 18:23:14 +0000

    config de OrderCrudControllers

--- src/Controller/Admin/OrdersCrudController.php
+++ src/Controller/Admin/OrdersCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Orders;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class OrdersCrudController extends AbstractCrudController
 {
@@ -12,14 +17,20 @@ class OrdersCrudController extends AbstractCrudController
         return Orders::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('reference'),
+            DateTimeField::new('createAt')->hideOnForm(),
+            AssociationField::new('users'),
         ];
     }
-    */
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityInstance->setCreateAt(new \DateTimeImmutable);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
 }
