d3188e1fa3a5fb5956cdd08edc07a117757ffba1
ozymandias54 <bassirou718@gmail.com>
2023-01-06 15:50:32 +0000

    gestion de la liste des produits

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
+use App\Repository\ImagesRepository;
+use App\Repository\ProductsRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'shop')]
-    public function index(): Response
+    public function index(ManagerRegistry $registry): Response
     {
+
+        $product = new ImagesRepository($registry);
+        $products = $product->findAll();
+        $categorie = new CategoriesRepository($registry);
+        $list = $categorie->findAll();
         return $this->render('shop/index.html.twig', [
-            'controller_name' => 'ShopController',
+            'categories' => $list,
+            'products' => $products,
         ]);
     }
 }
