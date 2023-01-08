196959ad414cf7e1d454496094be7f5b3564437a
ozymandias54 <bassirou718@gmail.com>
2023-01-08 15:22:08 +0000

    config de la page Shop-Detail

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductsRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DetailController extends AbstractController
 {
-    #[Route('/detail', name: 'detail')]
-    public function index(): Response
+    #[Route('/detail/{id}', name: 'detail')]
+    public function index($id, ManagerRegistry $registry): Response
     {
+        $product = new ProductsRepository($registry);
+        $product = $product->find($id);
         return $this->render('detail/index.html.twig', [
-            'controller_name' => 'DetailController',
+            'product' => $product,
         ]);
     }
 }
